9334fd5a4bb2c1d443171ed8eedd5e6a295667c2
ianratliff1 <ratlifim@gmail.com>
2022-09-04 09:09:48 +0200

    Added style to the Flirtinator - background, footer, css styling

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 var generateButton = document.getElementById('generate')
 var messageBox = document.getElementById('flirtyMessage')
 
+window.onkeypress = function(event) {
+      if (event.which == 32) {
+        flirt()
+      }
+    }
+
 function flirt() {
-    let greeting = ['Hayyyy','Why hello there','Hi','Hey ','Ciao','Sup','Howdy','What\'s cookin\'']
-    let noun = ['you sexy thang', 'good lookin\'','you absolute specimen','baby','darlin\'','sweetie','bella']
-    let action = ['grab a drink','meet for coffee','get to know each other better','go on a date','go eat samples at Costco','go out for a bello apperitivo']
-    let timeframe = ['after work','tomorrow','this weekend','Friday night','after church','after school']
-    let greetingIndex = Math.floor(Math.random()*greeting.length)
-    let nounIndex = Math.floor(Math.random()*noun.length)
-    let actionIndex = Math.floor(Math.random()*timeframe.length)
-    let timeframeIndex = Math.floor(Math.random()*action.length)
+    const greeting = ['Hayyyy','Why hello there','Hi','Hey ','Ciao','Sup','Howdy','What\'s cookin\'']
+    const noun = ['you sexy thang', 'good lookin\'','you absolute specimen','baby','darlin\'','sweetie','bella']
+    const action = ['grab a drink','meet for coffee','get to know each other better','go on a date','go eat samples at Costco','go out for a bello apperitivo']
+    const timeframe = ['after work','tomorrow','this weekend','Friday night','after church','after school']
+    const greetingIndex = Math.floor(Math.random()*greeting.length)
+    const nounIndex = Math.floor(Math.random()*noun.length)
+    const actionIndex = Math.floor(Math.random()*timeframe.length)
+    const timeframeIndex = Math.floor(Math.random()*action.length)
     // messageBox.innerHTML=`${greetingIndex} ${nounIndex} ${actionIndex} ${timeframeIndex}`
     // messageBox.innerHTML=greeting.length
-    const flirtyMessage = `${greeting[greetingIndex]} ${noun[nounIndex]}, would you like to ${action[actionIndex]} ${timeframe[timeframeIndex]}?`
+    const flirtyMessage = `<h2>${greeting[greetingIndex]} ${noun[nounIndex]}, would you like to ${action[actionIndex]} ${timeframe[timeframeIndex]}?</h2>`
     messageBox.innerHTML=flirtyMessage
 }
+
